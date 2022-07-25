182898fb9570cc5dd3ed92f73d17a9122e7499c7
Matthieu Mota <matthieumota@gmail.com>
2022-07-25 17:24:00 +0200

    TP car

--- 01-bases/index.php
+++ 01-bases/index.php
@@ -28,5 +28,26 @@
     <p><?= $mina->playWith($bianca); ?></p>
 
     <p><?= $bianca->isChiped() ? 'Bianca est pucée' : 'Bianca n\'est pas pucée'; ?></p>
+
+    <h2 class="text-xl font-bold text-center">Exercice voiture</h2>
+
+    <?php
+        require 'Car.php';
+
+        $alfa = new Car('Alfa Roméo', '147', 'grise');
+        $bmw = new Car('BMW', 'Série 4 Gran Coupé', 'noire');
+    ?>
+
+    <p>Voiture 1 : <?= $alfa->details(); ?> est <?= $alfa->getColor(); ?></p>
+    <p>Voiture 2 : <?= $bmw->details(); ?> est <?= $bmw->getColor(); ?></p>
+    <p>La voiture 1 klaxonne : <?= $alfa->klaxon(); ?></p>
+    <p>La voiture 2 roule : <?= $bmw->drive(); ?></p>
+
+    <?php while ($bmw->hasFuel()) { ?>
+        <p>La voiture 2 roule : <?= $bmw->drive(); ?></p>
+    <?php } ?>
+
+    <p>La voiture 2 roule : <?= $bmw->drive(); ?></p>
+    <p>La voiture 2 roule : <?= $bmw->fill(10)->fill(10)->fill(10)->drive(); ?></p>
 </body>
 </html>
