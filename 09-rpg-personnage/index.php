e2cb8d2a21c965e12ce3a258ffdfce55df27a7cf
Matthieu Mota <matthieumota@gmail.com>
2022-07-28 16:18:46 +0200

    Check errors

--- 09-rpg-personnage/index.php
+++ 09-rpg-personnage/index.php
@@ -9,6 +9,10 @@ $character = null;
 
 if (submit()) {
     $character = new Character(post('name'), post('class'), post('tribe'));
+
+    if (post('random')) {
+        $character->randomName();
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -22,8 +26,35 @@ if (submit()) {
 </head>
 <body class="font-[Lato]">
     <div class="max-w-3xl mx-auto mt-8">
+        <?php if ($character && !$character->hasErrors()) { ?>
+        <div class="border p-8 rounded-lg mb-8 text-center">
+            <h1 class="text-2xl">Salut <?= $character->name; ?></h1>
+            <p>Tu es un <?= $character->getClass().' '.$character->getTribe(); ?>.</p>
+
+            <img class="my-4 mx-auto rounded-full" src="<?= $character->image(); ?>" alt="<?= $character->name; ?>">
+
+            <ul class="divide-y border mb-4 w-1/2 mx-auto rounded">
+                <li class="py-2">Ta santé: <?= $character->health; ?></li>
+                <li class="py-2">Ta force: <?= $character->strength; ?></li>
+                <li class="py-2">Ton mana: <?= $character->mana; ?></li>
+            </ul>
+
+            <div class="text-center">
+                <a href="index.php" class="bg-blue-500 hover:bg-blue-600 duration-300 text-white px-3 py-2 rounded inline-block">Je veux un autre personnage</a>
+            </div>
+        </div>
+        <?php } ?>
+
         <div class="border p-8 rounded-lg">
             <h1 class="text-center text-2xl mb-6">POO RPG - Créer votre personnage</h1>
+
+            <?php if ($character && $character->hasErrors()) { ?>
+                <ul class="bg-red-300 text-red-900 mb-8 px-6 py-5 rounded-lg">
+                    <?php foreach ($character->errors() as $error) { ?>
+                        <li><?= $error; ?></li>
+                    <?php } ?>
+                </ul>
+            <?php } ?>
 
             <form action="" method="post" class="space-y-6">
                 <div>
--- 09-rpg-personnage/src/Character.php
+++ 09-rpg-personnage/src/Character.php
@@ -10,6 +10,16 @@ class Character
     public $health = 100;
     public $strength = 10;
     public $mana = 10;
+    public const CLASSES = [
+        'warrior' => 'Guerrier',
+        'magus' => 'Mage',
+        'hunter' => 'Chasseur',
+    ];
+    public const TRIBES = [
+        'human' => 'Humain',
+        'dwarf' => 'Nain',
+        'elf' => 'Elfe',
+    ];
 
     public function __construct($name = null, $class = null, $tribe = null)
     {
@@ -33,5 +43,51 @@ class Character
             $this->strength = 20;
             $this->mana = 20;
         }
+    }
+
+    /**
+     * Permet de générer un nom aléatoire au personnage.
+     */
+    public function randomName()
+    {
+        $names = ['cloud', 'tifa', 'aeris', 'sephiroth', 'barret', 'fiorella', 'auron', 'tidus', 'yuna', 'lulu', 'seymour', 'rikku', 'wakka', 'kimahri'];
+
+        $this->name = ucfirst($names[array_rand($names)]);
+
+        return $this;
+    }
+
+    /**
+     * Permet de remplir un tableau avec les erreurs potentielles.
+     */
+    public function errors()
+    {
+        $errors = [];
+
+        if (empty($this->name)) {
+            $errors[] = 'Veuillez choisir un nom ou en générer un aléatoire.';
+        }
+
+        if (empty($this->tribe)) {
+            $errors[] = 'Veuillez choisir une tribu.';
+        } else if (! in_array($this->tribe, array_keys(self::TRIBES))) {
+            $errors[] = 'Veuillez choisir une tribu valide: '.implode(', ', self::TRIBES).'.';
+        }
+
+        if (empty($this->class)) {
+            $errors[] = 'Veuillez choisir une classe.';
+        } else if (! in_array($this->class, array_keys(self::CLASSES))) {
+            $errors[] = 'Veuillez choisir une classe valide: '.implode(', ', self::CLASSES).'.';
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Permet de savoir s'il y a des erreurs ou non.
+     */
+    public function hasErrors()
+    {
+        return ! empty($this->errors());
     }
 }
