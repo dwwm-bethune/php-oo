1b2a89b8c02a353bd14748714e768525727a8e05
Matthieu Mota <matthieumota@gmail.com>
2022-07-28 15:45:00 +0200

    Prepare composer, img, class, helpers

---
+++ 09-rpg-personnage/index.php
@@ -0,0 +1,4 @@
+<?php
+
+require 'src/helpers.php';
+autoload();
---
+++ 09-rpg-personnage/src/Character.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Game;
+
+class Character
+{
+    public $name;
+    public $class;
+    public $tribe;
+    public $health = 100;
+    public $strength = 10;
+    public $mana = 10;
+}
